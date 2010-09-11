953567fb968d10ae8769babfcc0322130f40578d
tjarratt <tjarratt@gmail.com>
2010-09-11 16:51:59 -0700

    we need to store the uuid we've created so there is a record of this auth session.

--- auth.js
+++ auth.js
@@ -33,8 +33,14 @@ exports.login = function(request, response) {
 					return false;
 				}
 				var uuid = Math.uuid();
-				response.writeHead(200, {'Content-Type': 'text/plain'});
-				response.end(uuid);
+				
+				//I would RATHER use memcached for this, so we can expire the keys, but this works for now
+				client.hset("login", uuid, user, function (e, result) {
+					if (e) {errorHandler.err(500, "Internal Error while authenticating."); return false;}
+					
+					response.writeHead(200, {'Content-Type': 'text/plain'});
+					response.end(uuid);
+				});
 			});
 		}
 	});
